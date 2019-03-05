7472806c0c7aa3511791967b345081e7506a9fa1
franklindesign <manghi@gmail.com>
2019-03-04 19:03:05 -0800

    Updates

---
+++ precache-manifest.f4e8ba83020c0dcc34c692fee34fb720.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "345cee5d43ce86d01eb3",
+    "url": "/react-contacts-crud/static/css/main.c6bff4f2.chunk.css"
+  },
+  {
+    "revision": "345cee5d43ce86d01eb3",
+    "url": "/react-contacts-crud/static/js/main.345cee5d.chunk.js"
+  },
+  {
+    "revision": "69d5cb433d52c8bd7861",
+    "url": "/react-contacts-crud/static/js/runtime~main.69d5cb43.js"
+  },
+  {
+    "revision": "8ab7dd8eddb2eff7df5d",
+    "url": "/react-contacts-crud/static/css/2.6c598906.chunk.css"
+  },
+  {
+    "revision": "8ab7dd8eddb2eff7df5d",
+    "url": "/react-contacts-crud/static/js/2.8ab7dd8e.chunk.js"
+  },
+  {
+    "revision": "83f48f7d88e6cbb13ed7388d2103108f",
+    "url": "/react-contacts-crud/index.html"
+  }
+];
